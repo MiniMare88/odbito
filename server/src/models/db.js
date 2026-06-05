5b02813c6f916d71c5f422a2aab04b40c90f2357
Marko Knafelc <m.knafelc@gmail.com>
2026-06-05 13:48:27 +0200

    Add SSL support for Railway PostgreSQL connection

--- server/src/models/db.js
+++ server/src/models/db.js
@@ -4,6 +4,9 @@ export const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
   define: { underscored: true, timestamps: true },
+  dialectOptions: {
+    ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false,
+  },
 })
 
 export default sequelize
